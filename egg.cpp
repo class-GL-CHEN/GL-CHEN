15eb102dd2c4b2689af193a1667c7b110879d49e
GL-CHEN <cc820501@hotmail.com>
2015-03-18 08:45:35 +0800

    彩蛋作業2

--- egg.cpp
+++ egg.cpp
@@ -3,7 +3,11 @@
 int main()
 {
 	int i;
-	for(i=0;i<10;++i)
+	for(i=0;i<4;++i)
+	{
+		printf("egg-%d\n", i );	// please print egg-1, egg-2, egg-3, ..., egg-9
+	}
+	for(i=4;i>2;--i)
 	{
 		printf("egg-%d\n", i );	// please print egg-1, egg-2, egg-3, ..., egg-9
 	}
